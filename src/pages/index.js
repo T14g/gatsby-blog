2758a94523693a68810ec8c09966e444a3ee1903
Tiago <ottiagos@gmail.com>
2020-07-03 01:03:14 -0300

    Blog basic structure

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,65 @@
 import React from "react"
-import { Link } from "gatsby"
+import {graphql, Link } from "gatsby"
+import styled from "styled-components";
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
+const BlogLink = styled(Link)`
+  text-decoration: none;
+`;
+
+const BlogTitle = styled.h4`
+  color:blue;
+  margin-bottom:20px;
+`;
+
+export default ({data}) => (
   <Layout>
     <SEO title="Home" />
+    <div><h1>T14g Thoughts</h1></div>
     <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+
+    <h4>{data.allMarkdownRemark.totalCount}</h4>
+
+    {
+      data.allMarkdownRemark.edges.map(({node}) => (
+        <div key={node.id}>
+          <BlogLink to={node.fields.slug}>
+            <BlogTitle>{node.frontmatter.title} - {node.frontmatter.date}</BlogTitle>
+          </BlogLink>
+          <p>{node.excerpt}</p>
+        </div>
+      )
+    )
+  }
   </Layout>
 )
 
-export default IndexPage
+//Gatsby know you wanna pass to the default component
+
+export const query = graphql`
+    query {
+      file {
+        id
+      }
+      allMarkdownRemark(sort : { fields: [frontmatter___date], order: DESC}) {
+        totalCount
+        edges {
+          node {
+            id
+            frontmatter {
+              date
+              description
+              title
+            }
+            fields {
+              slug
+            }
+            excerpt
+          }
+        }
+      }
+    }
+`;
---
+++ src/pages/page-3.js
@@ -0,0 +1,10 @@
+import React from "react"
+import Layout from "../components/layout"
+
+const ThirdPage = () => (
+  <Layout>
+      <h1>hEIL 3RD Page</h1>
+  </Layout>
+)
+
+export default ThirdPage
